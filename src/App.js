4810823404b033c0930a25c18730d060bde1b8c6
Ahamed Rashad <ahamedrashad09@gmail.com>
2023-03-08 20:04:59 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Calculator() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (event) => {
+    const value = event.target.value;
+
+    if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Error');
+      }
+    } else {
+      setInput(input + value);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={input} />
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <button value="7" onClick={handleClick}>7</button>
+        <button value="8" onClick={handleClick}>8</button>
+        <button value="9" onClick={handleClick}>9</button>
+        <button value="/" onClick={handleClick}>÷</button>
+        <button value="4" onClick={handleClick}>4</button>
+        <button value="5" onClick={handleClick}>5</button>
+        <button value="6" onClick={handleClick}>6</button>
+        <button value="*" onClick={handleClick}>×</button>
+        <button value="1" onClick={handleClick}>1</button>
+        <button value="2" onClick={handleClick}>2</button>
+        <button value="3" onClick={handleClick}>3</button>
+        <button value="-" onClick={handleClick}>-</button><br/>
+        <button value="0" onClick={handleClick}>0</button>
+        <button value="." onClick={handleClick}>.</button>
+        <button value="+" onClick={handleClick}>+</button>
+        <button value="C" onClick={handleClick}>C</button>
+        <button value="=" onClick={handleClick}>=</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
